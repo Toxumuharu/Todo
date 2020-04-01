2b2ae010bc35a7a669d1e3e6cde02736b091911d
Tomoharu.Kawata <tomoharu.kawata@brilliantservice.co.jp>
2020-04-01 17:35:19 +0900

    thumbsup

--- Playgrounds.playground/Pages/CheckBox.xcplaygroundpage/Contents.swift
+++ Playgrounds.playground/Pages/CheckBox.xcplaygroundpage/Contents.swift
@@ -4,23 +4,45 @@ import SwiftUI
 import PlaygroundSupport
 import TodoFramework
 
-struct CheckBox : View {
-    @State var checked: Bool = false
+struct CheckBox<Label> : View where Label : View{
+    
+    @Binding private var checked: Bool
+    
+    private var label: ()->Label
+    
+    init(checked: Binding<Bool>,
+         @ViewBuilder label: @escaping ()->Label
+    ){
+        self._checked = checked
+        self.label = label
+    }
+    
     var body : some View{
-        VStack{
-            Toggle(isOn: $checked){
-                Text("チェックボックス")
+        HStack{
+            Image(systemName: checked ? "checkmark.circle" : "circle")
+                .onTapGesture{
+                    self.checked.toggle()
             }
-            if checked {
-                Text("Now it's ON!!")
-            }
+            label()
         }
     }
 }
 
 struct Previews : View {
     var body: some View{
-            CheckBox()
+        VStack{
+            CheckBox(checked: .constant(true)){
+                VStack{
+                    Text("task")
+                    Text("task")
+                    Text("task")
+                }
+                Image(systemName: "hand.thumbsup")
+            }
+            CheckBox(checked: .constant(false)){
+                Text("task")
+            }
+        }
     }
 }
 
